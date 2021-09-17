ba6f937705e5fb9b4af3b91035f0388e45b0c372
Horlamide <olamide.jubril@taxtech.com.ng>
2021-09-17 09:04:30 +0100

    [Form API] Adding form

--- boards/urls.py
+++ boards/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name='home'),
+    path('<int:pk>/new_topic/', views.new_topic, name='new_topic'),
+    path('board/<int:pk>/', views.board_topics, name='board_topics'),
 ]
--- boards/views.py
+++ boards/views.py
@@ -1,11 +1,45 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http.response import Http404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
+from .forms import NewTopicForm
 
 # Models
-from boards.models import Board
+from boards.models import Board, Topic, Post
 
 
 def home(request):
     boards = Board.objects.all()
 
     return render(request, 'home.html', {"boards": boards})
+
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics.html', {'board': board})
+
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first()  # TODO: get currently logged in user
+
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            # topic = form.save(commit=False)
+            # topic.board = board
+            # topic.user = user
+            topic = form.save()
+
+        # post = Post.objects.create(
+        #     message=form.cleaned_data.get('message'),
+        #     topic=topic,
+        #     created_by=user,
+        # )
+
+        # TODO: redirect to the created topic page
+        return redirect('board_topics', pk=board.pk)
+    else:
+        form = NewTopicForm()
+
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
