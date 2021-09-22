6da623d73075100d9af21f4c99e91e697fd5f695
Horlamide <olamide.jubril@taxtech.com.ng>
2021-09-22 23:24:01 +0100

    [Logout] Implementing logout

--- accounts/tests/tests_view_signup.py
+++ accounts/tests/tests_view_signup.py
@@ -1,8 +1,10 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import resolve, reverse
-from . views import signup
+
+
+from ..views import signup
+from ..forms import SignUpForm
 
 # Create your tests here.
 
@@ -24,7 +26,17 @@ class SignUpTests(TestCase):
 
     def test_contains_form(self):
         form = self.response.context.get('form')
-        self.assertIsInstance(form, UserCreationForm)
+        self.assertIsInstance(form, SignUpForm)
+
+    def test_form_inputs(self):
+        '''
+        The view must contain five inputs: csrf, username, email,
+        password1, password2
+        '''
+        self.assertContains(self.response, '<input', 5)
+        self.assertContains(self.response, 'type="text"', 1)
+        self.assertContains(self.response, 'type="email"', 1)
+        self.assertContains(self.response, 'type="password"', 2)
 
 
 class SuccessfulSignUpTests(TestCase):
@@ -39,16 +51,16 @@ class SuccessfulSignUpTests(TestCase):
         self.response = self.client.post(url, data)
         self.home_url = reverse('home')
 
-    def test_redirection(self): # Test Failed
+    def test_redirection(self):  # Test Failed
         '''
         A valid form submission should redirect the user to the home page
         '''
         self.assertRedirects(self.response, self.home_url)
 
-    def test_user_creation(self): # Test Failed
+    def test_user_creation(self):  # Test Failed
         self.assertTrue(User.objects.exists())
 
-    def test_user_authentication(self): # Test Failed
+    def test_user_authentication(self):  # Test Failed
         '''
         Create a new request to an arbitrary page.
         The resulting response should now have a `user` to its context,
@@ -71,7 +83,7 @@ class InvalideSignUpTests(TestCase):
         '''
         self.assertEqual(self.response.status_code, 200)
 
-    def test_form_error(self): # Test Failed
+    def test_form_error(self):  # Test Failed
         form = self.response.context.get('form')
         self.assertTrue(form.errors)
 
